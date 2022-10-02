0e99a02db491e7f8496a988ed9c60b9db59fcf1c
Anton Ilyin <ilyin.a.e@gmail.com>
2022-10-02 23:05:33 +0300

    Modified Ex043

--- Ex043/Program.cs
+++ Ex043/Program.cs
@@ -11,6 +11,18 @@ Console.WriteLine("Введите B1:"); double b1 = Convert.ToDouble(Console.Re
 Console.WriteLine("Введите K2:"); double k2 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите B2:"); double b2 = Convert.ToDouble(Console.ReadLine());
 
+if ((b1 == b2) && (k1 == k2))
+{
+    Console.WriteLine ("Решений бесконечное множество - введены параметры одной и той же прямой");
+    return;
+}
+if (k1 == k2)
+{
+    Console.WriteLine ("Прямые параллельны - решений не существует");
+    return;
+}
+
 double outX = (b2-b1)/(k1-k2);
 double outY = k1*outX + b1;
+
 Console.WriteLine($"Решение системы уравнений: X = {outX}, Y = {outY}");
